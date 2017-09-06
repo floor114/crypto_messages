eedf32d563c685e5c07189fae150d267a836866f
Taras Shpachenko <taras.shpachenko@gmail.com>
2017-09-06 21:13:28 +0300

    Find messages.

--- app/concepts/message/operation/show.rb
+++ app/concepts/message/operation/show.rb
@@ -2,16 +2,9 @@ require_dependency 'message/operation/visit'
 
 class Message
   class Show < Trailblazer::Operation
-    step ::Trailblazer::Operation::Model(::Message, :find_by)
-    failure :not_found!
+    step ::Trailblazer::Operation::Nested(::Message::Find)
 
     success ::Trailblazer::Operation::Nested(::Message::Visit)
     success ::Trailblazer::Operation::Nested(::Message::Destroy)
-
-    failure ::TrailblazerHelpers::Steps::AlertsHandler
-
-    def not_found!(options)
-      options['error_message'] = I18n.t('message.actions.not_found')
-    end
   end
 end
